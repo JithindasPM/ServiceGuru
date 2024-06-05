684cfce0a22c2397fcb0e2608c3c688afc3ce54c
jithindas <jithindas.5796@gmail.com>
2024-06-05 15:36:57 +0530

    api for customer, add serializer and view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import authentication,permissions
+from api.serializers import *
+from django.db.models import Sum
 
 # Create your views here.
+
+class CustomerViewSetView(ModelViewSet):
+
+    authentication_classes=[authentication.TokenAuthentication]
+    permission_classes=[permissions.IsAdminUser]
+    queryset=Customer.objects.all()
+    serializer_class=CustomerSerilizer
+
+    def perform_create(self, serializer):
+        serializer.save(technician=self.request.user)
